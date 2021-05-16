a25b572c4ad430ea403326813209b6d79ccaa1a8
Levy Ronald <levyronald3@gmail.com>
2021-05-16 14:59:17 -0300

    Site Matemática

---
+++ adição.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<title>Adição</title>
+	<link rel="stylesheet" type="text/css" href="Adição.css">
+	<?php
+	if($_POST){
+	$n1 = $_POST['valor1'];
+	$n2 = $_POST['valor2'];
+	$soma = $n1 + $n2;
+	$resultado = $soma;
+	}
+
+	 ?>
+</head>
+<body>
+	<nav>
+		<a href=""><h1>Calculadora Rápida</h1></a>
+	</nav>
+
+	<div class="cont">
+	<form method="post">
+		<h1>Adição</h1>
+		<p class="Valor 1">
+			<input type="text" name="valor1" placeholder="Digite um valor" required="required"  id="n1">
+		</p>
+		<p class="Valor 2">
+			<input type="text" name="valor2" placeholder="Digite um valor" required="required" id="n2">
+		</p>
+		<p class="enviar">
+			<input type="submit" name="somar" value="Somar" onclick="">
+		</p>
+		<p class="Resultado">
+			<input type="number" name="Resultado" placeholder="resultado" id="Result" value="<?php echo $resultado ?>">
+		</p>
+		<p class="enviar">
+			<a href="home.php">Voltar</a>
+		</p>
+	</div>
+	</form>
+</body>
+</html>
---
+++ home.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<title>Home</title>
+	<link rel="stylesheet" type="text/css" href="home.css">
+</head>
+<body>
+	<nav>
+		<a href=""><h1>Calculadora Rápida</h1></a>
+	</nav>
+
+	<div class="cont">
+	<form name="formtest" action="" method="post">
+		<h1>Operações Básicas</h1>
+
+		<ul>
+	 		<li> <a href="adição.php"> ADIÇÃO </a></li>
+	 		<li> <a href="subtraçãol.php"> SUBTRAÇÃO </a></li>
+	 		<li> <a href="multiplicação.php"> MULTIPLICAÇÃO </a></li>
+	 		<li> <a href="divisão.php"> DIVISÃO </a></li> 
+	 	</ul>
+	</div>
+	</form>
+</body>
+</html>
